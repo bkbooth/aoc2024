31d1f550241ba88fcbada48bdbf3fd9265859c79
Ben Booth <hey@benbooth.dev>
2024-12-02 22:27:41 +1100

    Setup TypeScript

---
+++ day01/index.ts
@@ -0,0 +1,8 @@
+async function main() {
+	console.log('Hello, day 01');
+}
+
+main().catch((error) => {
+	console.error(process.env.NODE_ENV === 'production' ? error.stack || error.message : error);
+	process.exit(1);
+});
