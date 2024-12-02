8c7cc43814d6d2753961d758c1b4bc16a7acce36
Ben Booth <hey@benbooth.dev>
2024-12-02 22:47:13 +1100

    Solve day 1, part 1

--- day01/index.ts
+++ day01/index.ts
@@ -1,5 +1,36 @@
+import { isEmpty } from 'lodash-es';
+import fs from 'node:fs/promises';
+import path from 'node:path';
+
+const INPUT_FILE = path.join(__dirname, 'input.txt');
+
 async function main() {
-	console.log('Hello, day 01');
+	const fileData = await fs.readFile(INPUT_FILE, { encoding: 'utf8' });
+
+	let listA: Array<number> = [];
+	let listB: Array<number> = [];
+
+	const lines = fileData.split('\n');
+	for (let i = 0, n = lines.length; i < n; i++) {
+		const line = lines[i];
+		if (isEmpty(line)) continue;
+
+		const [listAItem, listBItem] = line.split(/\s+/);
+		listA.push(Number.parseInt(listAItem));
+		listB.push(Number.parseInt(listBItem));
+	}
+	listA.sort((a, b) => a - b);
+	listB.sort((a, b) => a - b);
+
+	let totalDistance = 0;
+	for (let i = 0, n = listA.length; i < n; i++) {
+		const aItem = listA[i];
+		const bItem = listB[i];
+		const distance = Math.abs(aItem - bItem);
+		totalDistance += distance;
+	}
+
+	console.log('Total distance:', totalDistance);
 }
 
 main().catch((error) => {
