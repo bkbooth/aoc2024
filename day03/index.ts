e5c2dce91fe60bb986b0cd9d6bd7dcb8e26a4586
Ben Booth <hey@benbooth.dev>
2024-12-04 16:03:54 +1100

    Solve day 3, part 2

--- day03/index.ts
+++ day03/index.ts
@@ -3,22 +3,41 @@ import path from 'node:path';
 
 const INPUT_FILE = path.join(__dirname, 'input.txt');
 
-const MUL_COMMAND = /mul\((\d{1,3}),(\d{1,3})\)/;
-const MUL_COMMAND_GLOBAL = new RegExp(MUL_COMMAND, 'gm');
+const DO_INSTRUCTION = /do\(\)/;
+const DONT_INSTRUCTION = /don\'t\(\)/;
+const MUL_INSTRUCTION = /mul\((\d{1,3}),(\d{1,3})\)/;
+const ALL_INSTRUCTIONS = new RegExp(
+	DO_INSTRUCTION.source + '|' + DONT_INSTRUCTION.source + '|' + MUL_INSTRUCTION.source,
+	'gm'
+);
 
-function processMulCommand(mulCommand: string): number {
-	const result = MUL_COMMAND.exec(mulCommand);
-	const num1 = Number.parseInt(result[1]);
-	const num2 = Number.parseInt(result[2]);
-	return num1 * num2;
+let instructionsEnabled = true;
+function processInstruction(instruction: string): number {
+	if (DO_INSTRUCTION.test(instruction)) {
+		instructionsEnabled = true;
+		return 0;
+	} else if (DONT_INSTRUCTION.test(instruction)) {
+		instructionsEnabled = false;
+		return 0;
+	} else if (!instructionsEnabled) {
+		return 0;
+	} else {
+		const result = MUL_INSTRUCTION.exec(instruction);
+		const num1 = Number.parseInt(result[1]);
+		const num2 = Number.parseInt(result[2]);
+		return num1 * num2;
+	}
 }
 
 async function main() {
 	const fileData = await fs.readFile(INPUT_FILE, { encoding: 'utf8' });
 
-	const mulCommands = fileData.match(MUL_COMMAND_GLOBAL);
+	const instructions = fileData.match(ALL_INSTRUCTIONS);
 
-	const total = mulCommands.reduce((total, mulCommand) => total + processMulCommand(mulCommand), 0);
+	const total = instructions.reduce(
+		(total, instruction) => total + processInstruction(instruction),
+		0
+	);
 
 	console.log('Multiplications total:', total);
 }
