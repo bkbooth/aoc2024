9c75b2d0ad634aba5651d40094b46233935ff52d
Ben Booth <hey@benbooth.dev>
2024-12-05 14:22:11 +1100

    Refactor day 4 solutions
    - Less loops
    - Named x, y variables instead of indices

--- day04/wordSearch.ts
+++ day04/wordSearch.ts
@@ -26,24 +26,17 @@ export function buildWordSearch(input: string): WordSearch {
 function walkDirection(
 	wordSearch: WordSearch,
 	wordToFind: string,
-	location: Coordinate,
-	direction: Coordinate
+	[xStart, yStart]: Coordinate,
+	[xDirection, yDirection]: Coordinate
 ): boolean {
 	for (let depth = 1, n = wordToFind.length; depth < n; depth++) {
-		const coordinate: Coordinate = [
-			location[0] + direction[0] * depth,
-			location[1] + direction[1] * depth,
-		];
-		if (
-			coordinate[1] < 0 ||
-			coordinate[1] >= wordSearch.length ||
-			coordinate[0] < 0 ||
-			coordinate[0] >= wordSearch[0].length
-		) {
-			// Gone out of bounds
+		const x = xStart + xDirection * depth;
+		const y = yStart + yDirection * depth;
+		if (y < 0 || y >= wordSearch.length || x < 0 || x >= wordSearch[y].length) {
+			// Out of bounds
 			return false;
 		}
-		if (wordSearch[coordinate[1]][coordinate[0]] !== wordToFind[depth]) {
+		if (wordSearch[y][x] !== wordToFind[depth]) {
 			// Word not found
 			return false;
 		}
@@ -52,23 +45,17 @@ function walkDirection(
 }
 
 export function solveWordSearch(wordSearch: WordSearch, wordToFind: string): number {
-	let startingLocations: Array<Coordinate> = [];
+	let appearances = 0;
+
 	for (let y = 0, n = wordSearch.length; y < n; y++) {
 		for (let x = 0, m = wordSearch[y].length; x < m; x++) {
 			if (wordSearch[y][x] === wordToFind[0]) {
-				startingLocations.push([x, y]);
-			}
-		}
-	}
-
-	let appearances = 0;
-
-	for (let i = 0, n = startingLocations.length; i < n; i++) {
-		const location = startingLocations[i];
-		for (let d = 0, m = DIRECTIONS.length; d < m; d++) {
-			const direction = DIRECTIONS[d];
-			if (walkDirection(wordSearch, wordToFind, location, direction)) {
-				appearances++;
+				for (let d = 0, l = DIRECTIONS.length; d < l; d++) {
+					const direction = DIRECTIONS[d];
+					if (walkDirection(wordSearch, wordToFind, [x, y], direction)) {
+						appearances++;
+					}
+				}
 			}
 		}
 	}
@@ -76,37 +63,27 @@ export function solveWordSearch(wordSearch: WordSearch, wordToFind: string): num
 	return appearances;
 }
 
-function checkXmas(wordSearch: WordSearch, location: Coordinate) {
+function checkXmas(wordSearch: WordSearch, [x, y]: Coordinate) {
+	if (wordSearch[y][x] !== 'A') return false;
+
 	const downDiagonal =
-		(wordSearch[location[1] - 1][location[0] - 1] === 'M' &&
-			wordSearch[location[1] + 1][location[0] + 1] === 'S') ||
-		(wordSearch[location[1] - 1][location[0] - 1] === 'S' &&
-			wordSearch[location[1] + 1][location[0] + 1] === 'M');
+		(wordSearch[y - 1][x - 1] === 'M' && wordSearch[y + 1][x + 1] === 'S') ||
+		(wordSearch[y - 1][x - 1] === 'S' && wordSearch[y + 1][x + 1] === 'M');
 
 	const upDiagonal =
-		(wordSearch[location[1] + 1][location[0] - 1] === 'M' &&
-			wordSearch[location[1] - 1][location[0] + 1] === 'S') ||
-		(wordSearch[location[1] + 1][location[0] - 1] === 'S' &&
-			wordSearch[location[1] - 1][location[0] + 1] === 'M');
+		(wordSearch[y + 1][x - 1] === 'M' && wordSearch[y - 1][x + 1] === 'S') ||
+		(wordSearch[y + 1][x - 1] === 'S' && wordSearch[y - 1][x + 1] === 'M');
 
 	return downDiagonal && upDiagonal;
 }
 
 export function solveXmasPuzzle(wordSearch: WordSearch): number {
-	let startingLocations: Array<Coordinate> = [];
-	for (let y = 1, n = wordSearch.length - 1; y < n; y++) {
-		for (let x = 1, m = wordSearch[y].length - 1; x < m; x++) {
-			if (wordSearch[y][x] === 'A') {
-				startingLocations.push([x, y]);
-			}
-		}
-	}
-
 	let appearances = 0;
 
-	for (let i = 0, n = startingLocations.length; i < n; i++) {
-		const location = startingLocations[i];
-		if (checkXmas(wordSearch, location)) appearances++;
+	for (let y = 1, n = wordSearch.length - 1; y < n; y++) {
+		for (let x = 1, m = wordSearch[y].length - 1; x < m; x++) {
+			if (checkXmas(wordSearch, [x, y])) appearances++;
+		}
 	}
 
 	return appearances;
